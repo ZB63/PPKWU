0dad75cac600e7390f3dccf98e45f73601b0eb14
Sebastian Paweloszek <spaweloszek63@gmail.com>
2020-10-20 14:45:27 +0200

    Get endpoint and function to process passed value

--- src/main/java/com/lab/PPKWU/controller/BasicController.java
+++ src/main/java/com/lab/PPKWU/controller/BasicController.java
@@ -1,7 +1,15 @@
 package com.lab.PPKWU.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BasicController {
+
+    @GetMapping("/{text}")
+    public String rev(@PathVariable String text) {
+        return "";
+    }
+
 }
