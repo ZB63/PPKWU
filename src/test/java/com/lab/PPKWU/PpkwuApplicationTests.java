170e73692e09ae5319def0fb1d76c3593071fd80
Sebastian Paweloszek <spaweloszek63@gmail.com>
2020-10-20 14:28:44 +0200

    Initial commit with empty Spring Boot project, created with Spring Initializr.

---
+++ src/test/java/com/lab/PPKWU/PpkwuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.PPKWU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpkwuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
